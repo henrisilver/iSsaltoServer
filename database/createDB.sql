3ad49980a45206ffa61047efd0b9031b0eded7ff
Henrique Silveira <hd2312@columbia.edu>
2016-10-13 18:34:09 -0300

    Added SQL scripts to create the PostgreSQL database tables and populate one of the tables with constant content

---
+++ database/createDB.sql
@@ -0,0 +1,29 @@
+-- Script para criação da base de dados a ser utilizada pelo sistema iSsalto.
+-- Utilizacao:
+-- $ heroku
+-- $ heroku pg:psql --app issalto DATABASE
+-- \i <PATH_PARA_ESTE_SCRIPT>
+DROP TABLE IF EXISTS TipoOcorrencia, Usuario, Ocorrencia CASCADE;
+
+CREATE TABLE TipoOcorrencia( -- Tabela que modela um tipo de ocorrencia
+	Id integer NOT NULL PRIMARY KEY, -- Identificador do tipo de ocorrencia
+	Nome varchar(45) NOT NULL -- Descricao textual (nome) do tipo de ocorrencia
+);
+
+CREATE TABLE Usuario( -- Tabela que modela um usuario do sistema
+	Username varchar(45) NOT NULL PRIMARY KEY, -- Username do usuario
+	Hash varchar(45) NOT NULL, -- hash criada pela API do facebook para autenticacao
+	RaioDeBusca real NOT NULL, -- raio a partir do ponto de interesse do usurario para busca de ocorrencias
+	PosX real NOT NULL, -- longitude do ponto de interesse do usuario
+	PosY real NOT NULL -- latitude do ponto de interesse do usuario
+);
+
+CREATE TABLE Ocorrencia( -- Tabela que modela uma ocorrencia registrada
+	Id integer NOT NULL PRIMARY KEY, -- Identificador da Ocorrencia
+	Username varchar(45) NOT NULL REFERENCES Usuario(Username), -- Username que registrou a ocorrencia
+	Tipo integer NOT NULL REFERENCES TipoOcorrencia(Id), -- Tipo da ocorrencia em questao
+	OcorrenciaTimestamp timestamp NOT NULL, -- Timestamp da ocorrencia
+	LocalizacaoX real NOT NULL, -- Longitude da ocorrencia
+	LocalizacaoY real NOT NULL, -- Latitude da ocorrencia
+	Descricao varchar(450) -- Descricao opcional da ocorrencia
+);
---
+++ database/insertConstData.sql
@@ -0,0 +1,12 @@
+-- Script para inserção das colunas de dados constantes na base de dados
+-- a ser utilizada pelo sistema iSsalto.
+-- Utilizacao:
+-- $ heroku
+-- $ heroku pg:psql --app issalto DATABASE
+-- \i <PATH_PARA_ESTE_SCRIPT>
+INSERT INTO TipoOcorrencia (Id, Nome) VALUES (0, 'Assalto');
+INSERT INTO TipoOcorrencia (Id, Nome) VALUES (1, 'Furto');
+INSERT INTO TipoOcorrencia (Id, Nome) VALUES (2, 'Invasao de Propriedade');
+INSERT INTO TipoOcorrencia (Id, Nome) VALUES (3, 'Assedio');
+INSERT INTO TipoOcorrencia (Id, Nome) VALUES (4, 'Sequestro');
+INSERT INTO TipoOcorrencia (Id, Nome) VALUES (5, 'Assassinato');
