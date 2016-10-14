394d33fbd4eb283fae764e3b496634ea5695a682
Henrique Silveira <hd2312@columbia.edu>
2016-10-14 01:36:23 -0300

    Created first draft of server, serving requests to find occurences based on a username. Also created SQL script to test this first draft of the server

--- app.py
+++ app.py
@@ -1,15 +1,56 @@
+import psycopg2
+import urlparse
 from flask import Flask
 app = Flask(__name__)
 
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse("postgres://lijoleyfufklmt:thhCUh_hMA8bN2gUwmvmefYdNL@ec2-54-235-254-199.compute-1.amazonaws.com:5432/dfopjhaf40r7qr")
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+cur = conn.cursor()
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "This is iSsalto. Welcome. Made by iSsaltantes."
 
+@app.route('/ocorrencias/u=<username>')
+def getOcorrencias(username):
+    try:
+        cur.execute("""SELECT * from Usuario where Username='{}';""".format(username))
+    except:
+        return "Failed to fetch from table."
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+    rows = cur.fetchall()
+    if len(rows) != 1:
+        return "Error: no such username."
+
+    user = rows[0]
+
+    #TODO: create class for user.
+    searchRadius = user[2]
+    posXUser = user[3]
+    posYUser = user[4]
+
+    cur.execute("""SELECT * from Ocorrencia where LocalizacaoX >= {} AND LocalizacaoX <= {} AND LocalizacaoY >= {} AND LocalizacaoY <= {}""".format(posXUser - searchRadius, posXUser + searchRadius, posYUser - searchRadius, posYUser + searchRadius))
+    rows = cur.fetchall()
+    result = "Ocorrencias:\n"
+    for row in rows:
+        result = result + "Id: " + str(row[0]) + ", Username: " + str(row[1]) + ", Tipo: " + str(row[2]) + ", Data: " + str(row[3]) + ", PosX: " + str(row[4]) + ", PosY: " + str(row[5]) + ", Descricao: " + str(row[6]) + "\n"
+    return result
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
+
---
+++ database/exemploServer.sql
@@ -0,0 +1,14 @@
+-- Script de testes da base de dados a ser utilizada pelo sistema iSsalto.
+-- Utilizacao:
+-- $ heroku
+-- $ heroku pg:psql --app issalto DATABASE
+-- \i <PATH_PARA_ESTE_SCRIPT>
+DELETE FROM Ocorrencia;
+DELETE from Usuario;
+INSERT INTO Usuario VALUES ('Cassio', 'fd3409cbae982', 10.0, 20.0, 20.0);
+INSERT INTO Usuario VALUES ('Fagner', 'abcdef1234567', 20.0, 30.0, 30.0);
+INSERT INTO Usuario VALUES ('Walter', '7654321fedbca', 5.5, 87.0, 42.0);
+INSERT INTO Usuario VALUES ('Guilherme', 'a1b2c3d4e5f67', 1.0, 40.0, 5.0);
+INSERT INTO Ocorrencia VALUES(1, 'Fagner', 0, '2016-12-29 07:20:14', 15.0, 25.0, 'Malandgragem meu... Roubaram meu celular');
+INSERT INTO Ocorrencia VALUES(2, 'Walter', 1, '2016-12-29 07:20:14', 98.1, 23.4, 'Furtaram meu PokemonGO :('); -- ERRO: PK repetida em Ocorrencia
+INSERT INTO Ocorrencia VALUES(3, 'Guilherme', 2, '2016-01-17 16:47:33', 20.0, 20.0, 'Invadiu a area, eu defendo!'); -- Erro: FK Username nao existe
